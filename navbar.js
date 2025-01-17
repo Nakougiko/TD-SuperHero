a0ae35426529db4c94b1ccf0942adab645ed7c40
lukas <goulois.lukas@gmail.com>
2025-01-17 20:28:54 +0100

    Nettoyage du code HTML et JavaScript pour améliorer la lisibilité et la structure

--- navbar.js
+++ navbar.js
@@ -1,18 +1,18 @@
 fetch("navbar.html")
-            .then((response) => response.text())
-            .then((html) => {
-                document.getElementById("navbar-placeholder").innerHTML = html;
+    .then((response) => response.text())
+    .then((html) => {
+        document.getElementById("navbar-placeholder").innerHTML = html;
 
-                // Identifier la page active en fonction de l'URL
-                const currentPage = window.location.pathname.split("/").pop(); // Récupère "index.html" ou "detail.html" etc...
-                const navLinks = document.querySelectorAll(".nav-link");
+        // Identifier la page active en fonction de l'URL
+        const currentPage = window.location.pathname.split("/").pop(); // Récupère "index.html" ou "detail.html" etc...
+        const navLinks = document.querySelectorAll(".nav-link");
 
-                navLinks.forEach((link) => {
-                    // Ajoutez la classe active si le href du lien correspond à la page actuelle
-                    if (link.getAttribute("href") === currentPage) {
-                        link.classList.add("active");
-                    } else {
-                        link.classList.remove("active"); // Nettoyage des autres liens
-                    }
-                });
-            });
+        navLinks.forEach((link) => {
+            // Ajoutez la classe active si le href du lien correspond à la page actuelle
+            if (link.getAttribute("href") === currentPage) {
+                link.classList.add("active");
+            } else {
+                link.classList.remove("active"); // Nettoyage des autres liens
+            }
+        });
+    });
